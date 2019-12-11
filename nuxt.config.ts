4bdf3afa446c145f59c60ac46bf0424b3cd1b263
lollipop-onl <lollipop@simoochee.net>
2019-12-11 20:44:06 +0900

    🔧 NuxtのRouterモードを変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import { Configuration } from '@nuxt/types'
 const config: Configuration = {
   mode: 'spa',
   srcDir: 'src',
-  buildModules: ['@nuxt/typescript-build']
+  buildModules: ['@nuxt/typescript-build'],
+  router: {
+    base: './',
+    mode: 'hash'
+  }
 }
 
 module.exports = config
