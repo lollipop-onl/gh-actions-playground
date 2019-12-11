ce4cf5e6be4d496dafb664bdc124feaab0726b5a
lollipop-onl <lollipop@simoochee.net>
2019-12-11 14:17:16 +0900

    ✨ Nuxtアプリケーションの環境を追加

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { Configuration } from '@nuxt/types';
+
+const config: Configuration = {
+  srcDir: 'src',
+  buildModules: ['@nuxt/typescript-build'],
+};
+
+module.exports = config;
---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello you.</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'nuxt-property-decorator';
+
+  @Component
+  export default class  extends Vue {}
+</script>
+
+<style scoped></style>
